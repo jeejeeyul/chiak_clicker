2e5f7e52a978473bf623dafce24d4f09aaae9875
jeejeeyul <jee200713@gmail.com>
2024-05-31 15:49:23 +0900

    배경 전환

---
+++ src/change_bg.js
@@ -0,0 +1,8 @@
+var bg = document.getElementById("bg");
+function change_bg(bg_src) {
+  bg.src = bg_src;
+}
+
+change_bg(
+  "https://png.pngtree.com/thumb_back/fw800/background/20230408/pngtree-school-classroom-background-image_2146974.jpg"
+);
